5a880548a3bf695447cce9d9cd38761083a61ac9
lavie <895530239@qq.com>
2021-01-14 01:54:16 +0800

    feat:  spport

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,10 @@
 /*
  * @Author: your name
  * @Date: 2021-01-12 16:38:01
- * @LastEditTime: 2021-01-12 17:13:56
- * @LastEditors: your name
+ * @LastEditTime: 2021-01-12 17:23:08
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /chicken-design/components/index.ts
  */
 export { default as Alert } from './alert';
+console.log(111);
---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-14 01:30:48
+ * @LastEditTime: 2021-01-14 01:30:48
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /chicken-design/gatsby-config.js
+ */
+module.exports = {
+    plugins: ['gatsby-theme-docz', 'gatsby-plugin-less'],
+  };
