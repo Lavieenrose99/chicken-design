a36c5b70f8eb92c2d541775b07e18a2e1e6c3839
lavie <895530239@qq.com>
2021-01-25 18:54:34 +0800

    feat:change

--- .docz/.cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-exports.components = {
-  "component---components-alert-index-mdx": () => import("./../../../../components/alert/index.mdx" /* webpackChunkName: "component---components-alert-index-mdx" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
-}
-
