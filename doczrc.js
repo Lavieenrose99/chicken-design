6bacc71a1eb310085906de2116e78b15b1719836
lavie <895530239@qq.com>
2021-01-25 19:23:59 +0800

    feat:change

--- doczrc.js
+++ doczrc.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-06-15 10:39:04
- * @LastEditTime: 2021-01-25 18:53:22
+ * @LastEditTime: 2021-01-25 19:23:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /chicken-design/doczrc.js
  */
 export default {
   src: 'components',
-  dist: '.docz', // 打包出来的文件目录名
-  title: 'chicken-design', // 站点标题
+  dist: 'doc-site', // 打包出来的文件目录名
+  title: 'chicken-desgin', // 站点标题
   typescript: true, // 组件源文件是通过typescript开发，需要打开此选项
 };
