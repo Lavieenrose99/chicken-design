3ef8103360587cd0dad4428e437101110a601621
lavie <895530239@qq.com>
2021-01-26 13:03:21 +0800

    feat:change

--- components/alert/demo/1-demo-basic.tsx
+++ components/alert/demo/1-demo-basic.tsx
@@ -1,11 +1,4 @@
-/*
- * @Author: your name
- * @Date: 2020-06-15 10:39:04
- * @LastEditTime: 2021-01-26 00:50:22
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: /chicken-design/components/alert/demo/1-demo-basic.tsx
- */
+
 import React from 'react';
 import Alert from 'chicken-design/lib/alert';
 import 'chicken-design/lib/alert/style';
