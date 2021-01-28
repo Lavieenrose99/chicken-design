ca7c58eef55ec00fdedfa055101b307600781521
lavie <895530239@qq.com>
2021-01-29 00:05:06 +0800

    feat:change

---
+++ .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/17d97b2fb5b0b21789ef7dc963d7f061.js
@@ -0,0 +1,10 @@
+import { HappyBox } from "../../../../../doc-comps";
+import BasicDemo from "../../../../../components/alert/demo/1-demo-basic";
+import BasicDemoCode from "!raw-loader!../../../../../components/alert/demo/1-demo-basic.tsx";
+import * as React from 'react';
+export default {
+  HappyBox,
+  BasicDemo,
+  BasicDemoCode,
+  React
+};
---
+++ .docz/gatsby-config.custom.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-15 10:39:04
+ * @LastEditTime: 2021-01-28 17:30:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /chicken-design/gatsby-config.js
+ */
+module.exports = {
+  plugins: ['gatsby-theme-docz', 'gatsby-plugin-less', 'gatsby-plugin-styled-components','gatsby-plugin-sass'],
+};
