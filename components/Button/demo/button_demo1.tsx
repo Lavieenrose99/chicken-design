69aface8793798d43221ccba1badec3829907c90
lavie <895530239@qq.com>
2021-01-29 02:28:54 +0800

    feat:change

--- components/Button/demo/button_demo1.tsx
+++ components/Button/demo/button_demo1.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-28 17:42:39
- * @LastEditTime: 2021-01-29 01:44:08
+ * @LastEditTime: 2021-01-29 02:28:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /chicken-design/components/Button/demo/button_demo1.tsx
@@ -15,4 +15,4 @@ export default () => <>
 <Button disabled btnType="danger">危险按钮</Button> 
 <Button btnType="primary">基本按钮</Button>
 <Button btnType="default">默认按钮</Button>
-<Button disabled btnType="link">链接按钮</Button> </>;
+<Button disabled btnType="link">按钮</Button> </>;
