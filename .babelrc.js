91bc014b8ca979caf55267eaf30f21279f409527
lavie <895530239@qq.com>
2021-01-28 23:57:21 +0800

    feat:change

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-26 13:29:21
+ * @LastEditTime: 2021-01-28 23:57:13
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /chicken-design/.babelrc.js
+ */
 module.exports = {
-  presets: ['@babel/env', '@babel/typescript', '@babel/react'],
+  presets: ['@babel/env', '@babel/typescript', '@babel/react',' @babel/preset-react'],
   plugins: ['@babel/plugin-transform-runtime', '@babel/proposal-class-properties'],
   env: {
     esm: {
