4b1194fd8fe78e4fdb5791a4f5189987cd2eb44c
lavie <895530239@qq.com>
2021-02-01 00:57:45 +0800

    feat:change

--- lib/alert/interface.d.ts
+++ lib/alert/interface.d.ts
@@ -2,7 +2,6 @@ export declare type Kind = 'info' | 'positive' | 'negative' | 'warning';
 export declare type KindMap = Record<Kind, string>;
 export interface AlertProps {
     /**
-     * Set this to change alert kind
      * @default info
      */
     kind?: 'success' | 'info' | 'positive' | 'negative' | 'warning';
