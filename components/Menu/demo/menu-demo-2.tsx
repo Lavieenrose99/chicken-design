4843986bcee647d0b6cdc7f69557ca963f13b142
lavie <895530239@qq.com>
2021-02-16 00:01:41 +0800

    fix:icon

--- components/Menu/demo/menu-demo-2.tsx
+++ components/Menu/demo/menu-demo-2.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SubMenu from 'chicken-design/lib/Menu/subMenu';
 import Menu from 'chicken-design/lib/Menu/menu';
 import MenuItem from 'chicken-design/lib/Menu/menuItem';
-import 'chicken-design/lib/menu/styles'
+import 'chicken-design/lib/Menu/styles/index'
 
 
 export default ()=><div
--- components/alert/demo/5-demo-ne.tsx
+++ components/alert/demo/5-demo-ne.tsx
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-01-31 17:30:02
- * @LastEditTime: 2021-02-15 23:23:26
+ * @LastEditTime: 2021-02-16 00:00:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /chicken-design/components/alert/demo/5-demo-ne.tsx
  */
 import React from 'react';
 import Alert from 'chicken-design/lib/alert';
-import 'chicken-design/lib/alert/styles'
+import 'chicken-design/lib/alert/styles/index'
 
 
 export default () => <Alert kind="negative" title="ERROR" >出现了一些错误</Alert>;
