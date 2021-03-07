adf0898278f88cd9f2328e367f59928e97da124d
lavie <895530239@qq.com>
2021-03-07 18:59:36 +0800

    feat: 增加后台交互的评论

--- components/a_first/comment.jsx
+++ components/a_first/comment.jsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-07 13:33:02
- * @LastEditTime: 2021-03-07 18:56:51
+ * @LastEditTime: 2021-03-07 18:59:25
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /chicken-design/components/Input/comment.tsx
@@ -53,7 +53,7 @@ const Comment = () => {
         }>评论</Button>
         {!commentList.length ?
         <div style={{ textAlign: 'center' }}>
-        <Icon icon="spinner" spin />
+        <h2>加载中。。。</h2>
         </div> :
         <ol>{
 
